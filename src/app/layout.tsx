f77d879ab4f789754d7777bd5958659c7afa1d4e
张怡琳 <zhangyilin@shgbit.com>
2024-05-28 19:18:10 +0800

    feat：新增icon组件/全局header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.scss";
+import Icon from '@/components/Icon/Icon'
 
 export const metadata: Metadata = {
   title: "Romy Zhang",
@@ -16,7 +16,36 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="layout-header">
-          <div className="layout-header-img"></div>
+          <div className="layout-header-avatar">
+            <div className="layout-header-avatar-icon"></div>
+          </div>
+          <div className="layout-header-nav">
+            <div className="layout-header-nav-item">
+              <Icon name="home" />
+              <span className="layout-header-nav-item-text"> 首页</span>
+            </div>
+            <div className="layout-header-nav-item">
+              <Icon name="article" />
+              <span className="layout-header-nav-item-text"> 文稿</span>
+            </div>
+            <div className="layout-header-nav-item">
+              <Icon name="diary" />
+              <span className="layout-header-nav-item-text"> 日记</span>
+            </div>
+            <div className="layout-header-nav-item">
+              <Icon name="album" />
+              <span className="layout-header-nav-item-text"> 相册</span>
+            </div>
+            <div className="layout-header-nav-item">
+              <Icon name="time" />
+              <span className="layout-header-nav-item-text"> 时光</span>
+            </div>
+          </div>
+          <div className="layout-header-user">
+            <div className="layout-header-user-avatar">
+              <Icon name="user" />
+            </div>
+          </div>
         </div>
         <div>{children}</div>
       </body>
