be4d45c8049b1684ea6e57818eca0b4386fb05b9
张怡琳 <zhangyilin@shgbit.com>
2024-06-07 17:40:35 +0800

    feat: 调节首页样式

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,29 +2,28 @@ import icons from "@/components/Icon/iconSvg";
 import { ReactNode } from "react";
 import "./icon.scss";
 
-
 type params = {
-  name: string
-  size?: String | Number
-  width?: String | Number
-  height?: String | Number
-}
-
+  name: string;
+  size?: String | Number;
+  width?: String | Number;
+  height?: String | Number;
+};
 
 const Icon = ({ name, size, width, height }: params): ReactNode => {
-  const IconFont = icons[name]
+  const IconFont = icons[name];
 
   const style = {
-    width: (width || size) + 'px',
-    height: (height || size) + 'px'
-  }
+    width: (width || size) + "px",
+    height: (height || size) + "px",
+  };
 
   return (
-    <div className="common-icon" style={style}>
-      {IconFont}
-    </div>
-  )
-}
+    <div
+      className="common-icon"
+      style={style}
+      dangerouslySetInnerHTML={{ __html: IconFont }}
+    ></div>
+  );
+};
 
-
-export default Icon
+export default Icon;
